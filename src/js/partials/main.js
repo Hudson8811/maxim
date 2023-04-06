e4d639f4f32a309072517d2f5c74a5b040da71bb
KostaSenatov <kostyasenatov@gmail.com>
2023-04-06 19:09:51 +0400

    .

---
+++ src/js/partials/main.js
@@ -0,0 +1,28 @@
+var hero__swiper = new Swiper(".hero__swiper", {
+	spaceBetween: 100,
+	autoplay: {
+		delay: 5000,
+		disableOnInteraction: false,
+	},
+	loop: true,
+});
+
+
+var content__tabs = new Swiper(".content__tabs", {
+	spaceBetween: 10,
+	slidesPerView: 'auto',
+	freeMode: true,
+	watchSlidesProgress: true,
+	allowTouchMove: false,
+});
+var content__body_wrapper = new Swiper(".content__body-wrapper", {
+	allowTouchMove: false,
+	spaceBetween: 10,
+	navigation: {
+	nextEl: ".swiper-button-next",
+	prevEl: ".swiper-button-prev",
+	},
+	thumbs: {
+	swiper: content__tabs,
+	},
+});
