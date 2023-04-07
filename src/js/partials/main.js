d03e55351a1e4b26e70d43f07c7e0a8be8f9be98
PipZip <37911683+PipZip@users.noreply.github.com>
2023-04-07 15:59:12 +0600

    dinamic html

--- src/js/partials/main.js
+++ src/js/partials/main.js
@@ -1,167 +1,144 @@
-/* var hero__swiper = new Swiper(".hero__swiper", {
-	spaceBetween: 100,
-	autoplay: {
-		delay: 5000,
-		disableOnInteraction: false,
-	},
-	loop: true,
-}); */
+var heroSlider, contentTabs, contentInfo;
+
+$(document).ready(function() {
+    $.getJSON('/get_photos/', function(data) {
+        try {
+            JSON.parse(JSON.stringify(data));
 
 
-/* var content__tabs = new Swiper(".content__tabs", {
-	spaceBetween: 10,
-	slidesPerView: 'auto',
-	freeMode: true,
-	watchSlidesProgress: true,
-	allowTouchMove: false,
-});
-var content__body_wrapper = new Swiper(".content__body-wrapper", {
-	allowTouchMove: false,
-	spaceBetween: 10,
-	navigation: {
-	nextEl: ".swiper-button-next",
-	prevEl: ".swiper-button-prev",
-	},
-	thumbs: {
-	swiper: content__tabs,
-	},
-}); */
+            let html = data.map(function(img) {
+                return "<div class='swiper-slide'><img class='hero__image' src='" + img + "'></div>";
+            }).join('');
+
+            $(".hero__swiper .swiper-wrapper").html(html);
+
+            heroSlider = new Swiper(".hero__swiper", {
+                autoplay: {
+                    delay: 5000,
+                    disableOnInteraction: false,
+                },
+                loop: true,
+                effect: 'fade',
+                fadeEffect: {
+                    crossFade: true
+                },
+            });
+        } catch (e) {
+            console.error('Полученные данные не являются валидным JSON');
+        }
+    });
+
+    $.getJSON('/get_articles/', function(data) {
+        try {
+            JSON.parse(JSON.stringify(data));
+
+            let htmlTabTemplate = '<div class="swiper-slide"><div class="content__tab"><span>%name%</span></div></div>';
+
+            let htmlHeroTemplate = '<div class="content__item item-content"> \
+                                <div class="item-content__container"> \
+                                <div class="item-content__row"> \
+                                <div class="item-content__body"> \
+                                <div class="item-content__name">%title%</div> \
+                                <p class="item-content__text">%text%</p><a class="item-content__read-btn" href="%link%" target="_blank"> \
+                                ЧИТАТЬ ИСТОРИЮ</a> \
+                                <div class="item-content__like" data-id="%id%"> <img src="images/like.svg" alt="alt"><span>%likes%</span></div> \
+                                </div> \
+                                <div class="item-content__image"> <img src="%image%" alt="alt"></div> \
+                                </div> \
+                                </div> \
+                                </div>';
+
+            let htmlTemplate = '<div class="swiper-slide">\
+                                <div class="content__body"> \
+                                <div class="content__item item-content content__item--first">\
+                                <div class="item-content__container"> \
+                                <div class="item-content__title">%desc%</div>\
+                                <div class="item-content__title-big title-big"> <span>%name%</span></div>\
+                                <div class="item-content__row"> \
+                                <div class="item-content__body item-content__body-desktop"> \
+                                <p class="item-content__text">%text%</p>\
+                                </div>\
+                                <div class="item-content__image" style="background-image: url(%image%);"></div>\
+                                </div>\
+                                <div class="item-content__body item-content__body-mobile"> \
+                                <p class="item-content__text">%text%</p>\
+                                </div>\
+                                </div>\
+                                </div>\
+                                %hero%\
+                                </div>';
+
+            $.each(data, function(key, value) {
+                let heroes = value.heroes;
+                let herosHtml = '';
+                $.each(heroes, function(index, hero) {
+                    let heroHtml = htmlHeroTemplate;
+                    heroHtml = heroHtml.replace('%title%', hero.title);
+                    heroHtml = heroHtml.replace('%text%', hero.text);
+                    heroHtml = heroHtml.replace('%image%', hero.image);
+                    heroHtml = heroHtml.replace('%link%', hero.link);
+                    heroHtml = heroHtml.replace('%likes%', hero.likes);
+                    heroHtml = heroHtml.replace('%id%', hero.id);
+                    herosHtml += heroHtml;
+                });
+
+                let html = htmlTemplate;
+                html = html.replace('%name%', value.name);
+                html = html.replace('%desc%', value.desc);
+                html = html.replace(/%text%/g, value.text);
+                html = html.replace('%image%', value.image);
+                html = html.replace('%hero%', herosHtml);
+
+                let htmlTab = htmlTabTemplate;
+                htmlTab = htmlTab.replace('%name%', value.name);
+
+                $(".content__tabs .swiper-wrapper").append(htmlTab);
+                $(".content__body-wrapper .swiper-wrapper").append(html);
+            });
 
 
+            contentTabs = new Swiper(".content__tabs", {
+                spaceBetween: 10,
+                slidesPerView: 'auto',
+                freeMode: true,
+                watchSlidesProgress: true,
+                allowTouchMove: false,
+            });
+            contentInfo = new Swiper(".content__body-wrapper", {
+                allowTouchMove: false,
+                spaceBetween: 10,
+                thumbs: {
+                    swiper: contentTabs,
+                },
+            });
 
+        } catch (e) {
+            console.error('Полученные данные не являются валидным JSON');
+        }
+    });
 
-$(document).ready(function() { 
-	$(window).load(function() { 
-		
-		function getData1(social) {
-			$.ajax({
-				type: "POST",
-				url: "/get_photos/",
-				data: { social: social },
-				success: function (data) {
-					
-					arr = ['images/1.png', 'images/1.png', 'images/1.png'];
-
-					var parse = JSON.parse(data);
-					arr = parse;
-
-					var hero_swiper_wrapper = document.createElement('div');
-					$(".hero__swiper").append(hero_swiper_wrapper)
-					$('.hero__swiper >div').attr("class", "swiper-wrapper");
-
-					$.each(arr, function(i, arr) {
-						$(".hero__swiper .swiper-wrapper").append("<div class='swiper-slide'><img style='width: 100%;' src='"+arr+"'></div>");
-					})
-				},
-				error: function () {
-					alert('Ошибка запроса вопросов');
-				}
-			});
-			var hero__swiper = new Swiper(".hero__swiper", {
-				autoplay: {
-					delay: 5000,
-					disableOnInteraction: false,
-				},
-				loop: true,
-			});
-
-		}
-		getData1();
-
-
-
-		function getData2(social) {
-			$.ajax({
-				type: "POST",
-				url: "/get_articles/",
-				data: { social: social },
-				success: function (data) {
-					
-										arr = ['Спорт', 'музыка', 'Кино', 'Бизнес', 'Искусство', 'Юмор', 'Лидеры мнений'];
-
-					var parse = JSON.parse(data);
-					arr = parse;
-
-					var content_body_wrapper = document.createElement('div');
-					$(".content__body-wrapper").append(content_body_wrapper)
-					$('.content__body-wrapper >div').attr("class", "swiper-wrapper");
-
-					$.each(arr, function(i, arr) {
-						$(".content__body-wrapper .swiper-wrapper").append(
-							`
-							<div class="swiper-slide">
-								<div class="content__body"> 
-									<div class="content__item item-content content__item--first">
-									<div class="item-content__container"> 
-										<div class="item-content__title">Истории ярких спортсменов и тренеров России</div>
-										<div class="item-content__title-big title-big"> <span>${arr}</span></div>
-										<div class="item-content__row"> 
-											<div class="item-content__body item-content__body-desktop"> 
-											<p class="item-content__text">Спорт — это эмоции. Боль поражений, радость побед. Но еще спорт — это постоянная работа над собой. Преодоление обстоятельств, соперников, судей, своих барьеров. Как жить, влюбляться, воспитывать детей и при этом быть максимально сконцентрированным на результате — рассказывают наши спортивные герои.</p>
-											</div>
-											<div class="item-content__image"> </div>
-										</div>
-										<div class="item-content__body item-content__body-mobile"> 
-											<p class="item-content__text">Спорт — это эмоции. Боль поражений, радость побед. Но еще спорт — это постоянная работа над собой. Преодоление обстоятельств, соперников, судей, своих барьеров. Как жить, влюбляться, воспитывать детей и при этом быть максимально сконцентрированным на результате — рассказывают наши спортивные герои.</p>
-										</div>
-									</div>
-									</div>
-									<div class="content__item item-content"> 
-									<div class="item-content__container"> 
-										<div class="item-content__row"> 
-											<div class="item-content__body"> 
-											<div class="item-content__name">Роман Курцын</div>
-											<p class="item-content__text">Когда-то я решил, что пробиться в волшебный мир кино мне поможет отличная физическая форма. И… получилось!</p><a class="item-content__read-btn" href="">
-												ЧИТАТЬ ИСТОРИЮ</a>
-											<div class="item-content__like"> <img src="images/like.svg" alt="alt"><span>505</span></div>
-											</div>
-											<div class="item-content__image"> <img src="images/2.png" alt="alt"></div>
-										</div>
-									</div>
-									</div>
-									<div class="content__item item-content">
-									<div class="item-content__container"> 
-										<div class="item-content__row"> 
-											<div class="item-content__body"> 
-											<div class="item-content__name">Роман Курцын</div>
-											<p class="item-content__text">Когда-то я решил, что пробиться в волшебный мир кино мне поможет отличная физическая форма. И… получилось!</p><a class="item-content__read-btn" href="">
-												ЧИТАТЬ ИСТОРИЮ</a>
-											<div class="item-content__like"> <img src="images/like.svg" alt="alt"><span>505</span></div>
-											</div>
-											<div class="item-content__image"> <img src="images/2.png" alt="alt"></div>
-										</div>
-									</div>
-									</div>
-								</div>
-							</div>
-							`
-						);
-					})
-				},
-				error: function () {
-					alert('Ошибка запроса вопросов');
-				}
-			});
-			var content__tabs = new Swiper(".content__tabs", {
-				spaceBetween: 10,
-				slidesPerView: 'auto',
-				freeMode: true,
-				watchSlidesProgress: true,
-				allowTouchMove: false,
-			});
-			var content__body_wrapper = new Swiper(".content__body-wrapper", {
-				allowTouchMove: false,
-				spaceBetween: 10,
-				navigation: {
-				nextEl: ".swiper-button-next",
-				prevEl: ".swiper-button-prev",
-				},
-				thumbs: {
-				swiper: content__tabs,
-				},
-			});
-		}
-		getData2();
-	});
+    $(document).on('click','.item-content__like',function (){
+        event.preventDefault();
+        let elem = $(this)
+        if (!elem.hasClass('clicked')){
+            elem.addClass('clicked');
+            let id = $(this).data('id');
+            $.ajax({
+                type: "POST",
+                url: "/add_like/",
+                data: { id : id },
+                success: function(data) {
+                    if (data.length > 0) {
+                        elem.find('span').text(parseInt(data));
+                    } else {
+                        console.log("Пустой like");
+                    }
+                },
+                error: function () {
+                    console.log('Ошибка like');
+                }
+            });
+        }
+    });
 });
 
